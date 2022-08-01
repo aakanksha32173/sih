fa08755ed9857534983e647a9216ced746721522
Aakanksha <aakarushi27@gmail.com>
2022-08-01 14:39:05 +0530

    heekko

---
+++ ChatBotAPI.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+
+from main import chatWithBot
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def man():
+    return render_template('index.html')
+
+@app.route('/chat', methods=['GET', 'POST'])
+def chatBot():
+    chatInput = request.form['chatInput']
+    return jsonify(chatBotReply=chatWithBot(chatInput))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
